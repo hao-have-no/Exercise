9ec7b377681c0b174201fa697abd1189d62cc551
fenghao <fenghao@infisa.cn>
2019-09-23 18:21:21 +0800

    弹幕，深拷贝

---
+++ html/new/bulletScreen/index.js
@@ -0,0 +1,11 @@
+let data = [
+    {value: '周杰伦的听妈妈的话，让我反复循环再循环', time: 5, color: 'red', speed: 1, fontSize: 22},
+    {value: '想快快长大，才能保护她', time: 10, color: '#00a1f5', speed: 1, fontSize: 30},
+    {value: '听妈妈的话吧，晚点再恋爱吧！爱呢？', time: 15},
+];
+// value：代表弹幕的内容 (必填)
+// time：代表弹幕展现的时间 (必填)
+// color：代表弹幕文字的颜色
+// speed：代表弹幕飘过的速度
+// fontSize：代表弹幕文字的大小
+// opacity：代表弹幕文字的透明度
